d4a0ecb35bfa150e9da0e08fa8440785f09d1bea
Vibhor_S <vibhor_s@dellteam.com>
2019-02-23 09:34:07 +0530

    Binary Search Refactor

--- src/main/java/com/equals3/algorithms/arrays/BinarySearch.java
+++ src/main/java/com/equals3/algorithms/arrays/BinarySearch.java
@@ -2,54 +2,50 @@ package com.equals3.algorithms.arrays;
 
 public class BinarySearch {
 
-    private int[] backingArray;
-    private int nItems;
-
-    public BinarySearch(int size) {
-        backingArray = new int[size];
-        nItems = 0;
-    }
-
-    public int find(int searchKey) {
+    public static int find(int backingArray[], int searchKey) {
         int lowerBound = 0;
-        int upperBound = nItems - 1;
+        int upperBound = backingArray.length - 1;
         int curruntIndex;
-        while (true) {
+        while (lowerBound <= upperBound) {
             curruntIndex = (lowerBound + upperBound) / 2;
             if (backingArray[curruntIndex] == searchKey) {
                 return curruntIndex;
             } else {
                 if (backingArray[curruntIndex] < searchKey)
                     lowerBound = curruntIndex + 1; // it in upper half
-                else
+                else if (backingArray[curruntIndex] > searchKey)
                     upperBound = curruntIndex - 1; // its in lower half
             }
+
         }
-
-
+        // fallback case
+        return -1;
     }
 
     public static int recursiveBinarySearch(int arr[], int item, int left, int right) {
 
+        if(left<=right){
         int mid = (left + right) / 2;
 
         if (arr[mid] == item) {
             return mid;
         }
-        if (left > right) {
-            return -1;
-        } else if (arr[mid] > item) {
+        else if (arr[mid] > item) {
             return recursiveBinarySearch(arr, item, left, mid - 1);
-        } else if(arr[mid] < item) {
+        } else if (arr[mid] < item) {
             return recursiveBinarySearch(arr, item, mid + 1, right);
-
         }
+    }
         return -1;
+    
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 5, 8, 10, 14, 44, 77, 78, 99};
-        int result = recursiveBinarySearch(arr, 10, 0, arr.length - 1);
+        int[] arr = { 2, 5, 8, 10, 14, 44, 77, 78, 99 };
+        int result = BinarySearch.find(arr, 44);
+        int recursiveBinarySearch = BinarySearch.recursiveBinarySearch(arr, 78, 0, arr.length - 1);
         System.out.println(result);
+        System.out.println(recursiveBinarySearch);
+
     }
 }
